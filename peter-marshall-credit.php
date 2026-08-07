4ec1b049bb6303f0c826e70f3f8d3511c71db23c
Peter Marshall <pm@petermarshall.ca>
2026-08-07 14:57:43 -0400

    make widget draggable

--- peter-marshall-credit.php
+++ peter-marshall-credit.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name:  Designed by Peter Marshall
 Description:  Adds a credit banner to the dashboard landing page.
-Version:      0.1
+Version:      0.1.1
 Author:       Peter Marshall
 Author URI:   https://petermarshall.ca
 License:      GPL2
@@ -20,8 +20,6 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 
 $myUpdateChecker->setBranch('main');
 
-define( 'PM_CREDIT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
 add_action('wp_dashboard_setup', 'pm_credit_add_dashboard_widgets');
 
 function pm_credit_add_dashboard_widgets() {
@@ -30,24 +28,41 @@ function pm_credit_add_dashboard_widgets() {
 }
 
 function pm_credit_widget_credits() {
-?>
-<style>
-#pm_credit_widget .postbox-header {
-display: none;
+	?>
+	<div class="pm-credit-wrapper hndle">
+		<a href="https://petermarshall.ca/" target="_blank">
+			<img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . 'assets/petermarshall-glowing-cube-wordmark.avif' ) ?>" alt="Logo" />
+		</a>
+	</div>
+	<?php
 }
-#pm_credit_widget .inside {
-margin: 0;
-padding: 0;
-}
-#pm_credit_widget a {
-display: flex;
-}
-#pm_credit_widget img {
-width: 100%;
-height: auto;
--webkit-user-drag: none;
-}
-</style>
-<a href="https://petermarshall.ca/" target="_blank"><img src="<?php echo PM_CREDIT_PLUGIN_URL . 'assets/petermarshall-glowing-cube-wordmark.avif' ?>" /></a>
-<?php
+
+add_action('admin_head', 'pm_credit_widget_admin_styles');
+function pm_credit_widget_admin_styles() {
+    $screen = get_current_screen();
+    if ( $screen && $screen->id === 'dashboard' ) {
+        ?>
+        <style>
+			#pm_credit_widget .postbox-header {
+				display: none;
+			}
+			#pm_credit_widget {
+				cursor: move;
+			}
+			#pm_credit_widget .inside {
+				margin: 0;
+				padding: 0;
+			}
+			#pm_credit_widget a {
+				display: flex;
+				cursor: unset;
+			}
+			#pm_credit_widget img {
+				width: 100%;
+				height: auto;
+				-webkit-user-drag: none;
+			}
+        </style>
+        <?php
+    }
 }
